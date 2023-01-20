68b382f96ea20c681ad09fa6f43211559b1882e6
yaroslavkuzyk <y.kuzyk@wpt.dev>
2023-01-20 14:54:28 +0200

    fir

--- index.js
+++ index.js
@@ -2,12 +2,14 @@ import express from 'express'
 import mongoose from 'mongoose'
 import galleryRouter from './routers/galleryRouter.js'
 import fileUpload from 'express-fileupload'
+import cors from 'cors'
 
 const PORT = 8080
 const DB = 'mongodb+srv://admin:admin@cluster0.tetakbn.mongodb.net/?retryWrites=true&w=majority'
 
 const app = express()
 
+app.use(cors())
 app.use(express.json())
 app.use(express.static('static'))
 app.use(fileUpload({}))
